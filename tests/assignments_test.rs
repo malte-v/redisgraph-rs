7dd040e0844b067a9588ae080fd18faff83a9439
Malte Voos <voos.malte@gmail.com>
2020-02-18 18:58:47 +0100

    remove warnings in tests

--- tests/assignments_test.rs
+++ tests/assignments_test.rs
@@ -1,8 +1,7 @@
 mod common;
 
-use redisgraph::{RedisGraphResult, RedisString, result_set::{Node, Relation, Scalar}};
+use redisgraph::{RedisGraphResult};
 use serial_test::serial;
-use maplit::hashmap;
 
 use common::*;
 
@@ -30,7 +29,7 @@ fn test_tuple() {
 #[serial]
 fn test_vec() {
     with_graph(|graph| {
-        graph.mutate("CREATE (n1 { prop: 1 }), (n2 { prop: 2 }), (n3 { prop: 3 })");
+        graph.mutate("CREATE (n1 { prop: 1 }), (n2 { prop: 2 }), (n3 { prop: 3 })").unwrap();
         let vec: Vec<i64> = graph.query("MATCH (n) RETURN n.prop ORDER BY n.prop").unwrap();
         assert_eq!(vec[0], 1);
         assert_eq!(vec[1], 2);
@@ -42,7 +41,7 @@ fn test_vec() {
 #[serial]
 fn test_tuple_vec() {
     with_graph(|graph| {
-        graph.mutate("CREATE (n1 { num: 1, word: 'foo' }), (n2 { num: 2, word: 'bar' }), (n3 { num: 3, word: 'baz' })");
+        graph.mutate("CREATE (n1 { num: 1, word: 'foo' }), (n2 { num: 2, word: 'bar' }), (n3 { num: 3, word: 'baz' })").unwrap();
         let tuple_vec: Vec<(i64, String)> = graph.query("MATCH (n) RETURN n.num, n.word ORDER BY n.num").unwrap();
         assert_eq!(tuple_vec[0], (1, "foo".to_string()));
         assert_eq!(tuple_vec[1], (2, "bar".to_string()));
